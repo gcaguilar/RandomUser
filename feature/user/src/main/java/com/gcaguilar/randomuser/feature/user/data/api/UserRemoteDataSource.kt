bc44441afecf37f370cdbe3a515fb39db8afa986
gcaguilar <38892827+gcaguilar@users.noreply.github.com>
2025-04-24 18:36:03 +0200

    build: added api client implementation

---
+++ feature/user/src/main/java/com/gcaguilar/randomuser/feature/user/data/api/UserRemoteDataSource.kt
@@ -0,0 +1,32 @@
+package com.gcaguilar.randomuser.feature.user.data.api
+
+
+import io.ktor.client.HttpClient
+import io.ktor.client.request.parameter
+import io.ktor.client.request.url
+import io.ktor.http.HttpMethod
+
+private const val BASE_URL = "https://randomuser.me/api"
+private const val RESULTS = 10
+
+interface UserRemoteDataSource {
+    suspend fun getUsers(page: Int, seed: String): Result<List<UserResponse>>
+}
+
+class RandomUserApiClient(private val client: HttpClient) : UserRemoteDataSource {
+    override suspend fun getUsers(
+        page: Int,
+        seed: String
+    ): Result<List<UserResponse>> {
+        return client.fetch<RandomUserResponse> {
+            url(BASE_URL)
+            method = HttpMethod.Get
+            parameter("page", page)
+            parameter("results", RESULTS)
+            parameter("seed", seed)
+            parameter("format", "json")
+        }.map {
+            it.results
+        }
+    }
+}
