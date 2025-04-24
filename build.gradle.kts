931c0465654c867b2907346c36da10def81b6cf6
gcaguilar <38892827+gcaguilar@users.noreply.github.com>
2025-04-24 18:54:11 +0200

    feat: create localstore-api module

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,5 +5,6 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.jetbrains.kotlin.jvm) apply false
     kotlin("plugin.serialization") version "2.0.21" apply false
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,3 +23,4 @@ rootProject.name = "RandomUser"
 include(":app")
 include(":database")
 include(":feature:user")
+include(":localstore-api")
