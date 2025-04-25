0a8bb36250700d3b6642be88f65c771fd8682bb0
gcaguilar <38892827+gcaguilar@users.noreply.github.com>
2025-04-25 19:45:19 +0200

    feat: create list to show users

---
+++ feature/user/src/main/java/com/gcaguilar/randomuser/feature/user/presentation/ui/InfiniteLazyList.kt
@@ -0,0 +1,40 @@
+package com.gcaguilar.randomuser.feature.user.presentation.ui
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import com.gcaguilar.randomuser.feature.user.presentation.UserModel
+
+const val InfiniteListTag = "InfiniteList"
+
+@Composable
+fun InfiniteLazyList(
+    userList: List<UserModel>,
+    listState: LazyListState,
+    onClickUser: (String) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    LazyColumn(
+        modifier = modifier
+            .testTag(InfiniteListTag),
+        state = listState,
+    ) {
+        items(userList.size, key = { userList[it].uuid }) {
+            val user = userList[it]
+            UserCard(
+                name = "${user.name} ${user.surname}",
+                phone = user.phone,
+                picture = user.picture,
+                email = user.email,
+                onClickUser = { onClickUser(user.uuid) },
+            )
+            if (it != userList.size - 1) {
+                HorizontalDivider()
+            }
+        }
+    }
+}
+
