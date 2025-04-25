2c230e429bb4c04780d106707b79e3b9d758fe83
gcaguilar <38892827+gcaguilar@users.noreply.github.com>
2025-04-25 19:21:08 +0200

    feature: create base graph

--- app/src/main/java/com/gcaguilar/randomuser/MainActivity.kt
+++ app/src/main/java/com/gcaguilar/randomuser/MainActivity.kt
@@ -7,10 +7,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
 import com.gcaguilar.randomuser.ui.theme.RandomUserTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +17,19 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val navController = rememberNavController()
+
             RandomUserTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    NavHost(
+                        navController = navController,
+                        startDestination = "",
                         modifier = Modifier.padding(innerPadding)
-                    )
+                    ) {
+
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RandomUserTheme {
-        Greeting("Android")
     }
 }
