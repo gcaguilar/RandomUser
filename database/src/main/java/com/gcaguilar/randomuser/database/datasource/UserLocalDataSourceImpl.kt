85886c9311dcae757e9073737c190f117b64dd02
gcaguilar <38892827+gcaguilar@users.noreply.github.com>
2025-04-25 18:33:34 +0200

    fix: update naming of mapper and fix use of it

--- database/src/main/java/com/gcaguilar/randomuser/database/datasource/UserLocalDataSourceImpl.kt
+++ database/src/main/java/com/gcaguilar/randomuser/database/datasource/UserLocalDataSourceImpl.kt
@@ -1,7 +1,7 @@
 package com.gcaguilar.randomuser.database.datasource
 
 import com.gcaguilar.randomuser.database.dao.UserDao
-import com.gcaguilar.randomuser.database.mapper.toUserModelDetailed
+import com.gcaguilar.randomuser.database.mapper.toUserEntity
 import com.gcaguilar.randomuser.userlocalstorageapi.UserLocalDataSource
 import com.gcaguilar.randomuser.userlocalstorageapi.UserModelDetailed
 
@@ -9,7 +9,7 @@ class UserLocalDataSourceImpl(
     private val userDao: UserDao
 ): UserLocalDataSource {
     override suspend fun insertAll(users: List<UserModelDetailed>) {
-        val usersEntity = users.toUserModelDetailed()
+        val usersEntity = users.toUserEntity()
         userDao.insertAll(usersEntity)
     }
 }
