2ecd4819593427aa6e5704ce4f8ec80aa13d3144
gcaguilar <38892827+gcaguilar@users.noreply.github.com>
2025-04-25 19:45:49 +0200

    feat: create basic screen and listen flow of users

---
+++ feature/user/src/main/java/com/gcaguilar/randomuser/feature/user/presentation/RandomUserScreen.kt
@@ -0,0 +1,30 @@
+package com.gcaguilar.randomuser.feature.user.presentation
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.gcaguilar.randomuser.feature.user.presentation.ui.InfiniteLazyList
+import org.koin.androidx.compose.koinViewModel
+
+@Composable
+fun RandomUserScreen(
+    modifier: Modifier = Modifier,
+    viewModel: RandomUserViewModel = koinViewModel()
+) {
+    val state = viewModel.uiState.collectAsStateWithLifecycle()
+
+    Column(
+        modifier = modifier.fillMaxSize()
+    ) {
+        InfiniteLazyList(
+            userList = state.value.users,
+            listState = rememberLazyListState(),
+            onClickUser = {
+                //TODO
+            }
+        )
+    }
+}
