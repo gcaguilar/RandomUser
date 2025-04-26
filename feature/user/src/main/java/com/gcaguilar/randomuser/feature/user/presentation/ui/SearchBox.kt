bf656c60e4cf372944418b3c842780078160ea8d
gcaguilar <38892827+gcaguilar@users.noreply.github.com>
2025-04-26 19:51:43 +0200

    Revert "feat: debounce emision of text in searchbox"
    This reverts commit 63b880a6f7bb2c1715fcfbbeccbc50f48ac955a3.

--- feature/user/src/main/java/com/gcaguilar/randomuser/feature/user/presentation/ui/SearchBox.kt
+++ feature/user/src/main/java/com/gcaguilar/randomuser/feature/user/presentation/ui/SearchBox.kt
@@ -10,18 +10,12 @@ import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.delay
 
 @Composable
 fun SearchBox(
@@ -30,15 +24,6 @@ fun SearchBox(
     hint: String = "Search...",
     onValueChange: (String) -> Unit,
 ) {
-    var text by remember { mutableStateOf(value) }
-    var debouncedText by remember { mutableStateOf(value) }
-
-    LaunchedEffect(text) {
-        delay(300)
-        debouncedText = text
-        onValueChange(debouncedText)
-    }
-
     Box(
         modifier = modifier
             .fillMaxWidth()
@@ -57,9 +42,7 @@ fun SearchBox(
         ) {
             BasicTextField(
                 value = value,
-                onValueChange = { newText ->
-                    text = newText
-                },
+                onValueChange = onValueChange,
                 modifier = Modifier
                     .weight(1f)
                     .padding(horizontal = 16.dp)
