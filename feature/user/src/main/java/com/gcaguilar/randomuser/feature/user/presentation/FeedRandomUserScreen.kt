51d702977f66e78afb2034201d1b0dc84d25f24a
gcaguilar <38892827+gcaguilar@users.noreply.github.com>
2025-04-26 18:55:06 +0200

    feat: implement search in screen

--- feature/user/src/main/java/com/gcaguilar/randomuser/feature/user/presentation/FeedRandomUserScreen.kt
+++ feature/user/src/main/java/com/gcaguilar/randomuser/feature/user/presentation/FeedRandomUserScreen.kt
@@ -2,11 +2,14 @@ package com.gcaguilar.randomuser.feature.user.presentation
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.gcaguilar.randomuser.feature.user.presentation.ui.InfiniteLazyList
+import com.gcaguilar.randomuser.feature.user.presentation.ui.SearchBox
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
@@ -19,6 +22,11 @@ fun FeedRandomUserScreen(
     Column(
         modifier = modifier.fillMaxSize()
     ) {
+        SearchBox(
+            modifier = Modifier.padding(horizontal = 16.dp, vertical = 4.dp),
+            value = state.value.searchText,
+            onValueChange = { viewModel.handle(FeedUserIntent.TextChanged(it)) },
+        )
         InfiniteLazyList(
             userList = state.value.users,
             listState = rememberLazyListState(),
