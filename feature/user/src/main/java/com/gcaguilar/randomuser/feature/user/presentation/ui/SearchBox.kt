63b880a6f7bb2c1715fcfbbeccbc50f48ac955a3
gcaguilar <38892827+gcaguilar@users.noreply.github.com>
2025-04-26 19:37:09 +0200

    feat: debounce emision of text in searchbox

--- feature/user/src/main/java/com/gcaguilar/randomuser/feature/user/presentation/ui/SearchBox.kt
+++ feature/user/src/main/java/com/gcaguilar/randomuser/feature/user/presentation/ui/SearchBox.kt
@@ -10,12 +10,18 @@ import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.delay
 
 @Composable
 fun SearchBox(
@@ -24,6 +30,15 @@ fun SearchBox(
     hint: String = "Search...",
     onValueChange: (String) -> Unit,
 ) {
+    var text by remember { mutableStateOf(value) }
+    var debouncedText by remember { mutableStateOf(value) }
+
+    LaunchedEffect(text) {
+        delay(300)
+        debouncedText = text
+        onValueChange(debouncedText)
+    }
+
     Box(
         modifier = modifier
             .fillMaxWidth()
@@ -42,7 +57,9 @@ fun SearchBox(
         ) {
             BasicTextField(
                 value = value,
-                onValueChange = onValueChange,
+                onValueChange = { newText ->
+                    text = newText
+                },
                 modifier = Modifier
                     .weight(1f)
                     .padding(horizontal = 16.dp)
