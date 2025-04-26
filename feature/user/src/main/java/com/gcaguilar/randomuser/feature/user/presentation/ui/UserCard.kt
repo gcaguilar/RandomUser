6ca5ffefd9f80c21fa6a2b9d13973fdf502f9360
gcaguilar <38892827+gcaguilar@users.noreply.github.com>
2025-04-26 20:49:58 +0200

    test: add case when delete user

--- feature/user/src/main/java/com/gcaguilar/randomuser/feature/user/presentation/ui/UserCard.kt
+++ feature/user/src/main/java/com/gcaguilar/randomuser/feature/user/presentation/ui/UserCard.kt
@@ -16,6 +16,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
 
@@ -54,7 +55,7 @@ fun UserCard(
                 Text(phone)
             }
             IconButton(
-                modifier = Modifier,
+                modifier = Modifier.testTag("Delete${name}"),
                 onClick = onRemoveUser
             ) {
                 Icon(
