eff2aeec7a2f633cd6548e17f5dfa49363c1d19d
gcaguilar <38892827+gcaguilar@users.noreply.github.com>
2025-04-27 22:14:55 +0200

    feat: Move route to feature module

--- app/src/main/java/com/gcaguilar/randomuser/Routes.kt
+++
@@ -1,6 +0,0 @@
-package com.gcaguilar.randomuser
-
-object Routes {
-    const val FEED = "feed"
-    const val DETAIL = "detail/{itemId}"
-}
---
+++ feature/user/src/main/java/com/gcaguilar/randomuser/feature/navigation/Routes.kt
@@ -0,0 +1,6 @@
+package com.gcaguilar.randomuser.feature.navigation
+
+@kotlinx.serialization.Serializable
+object Feed
+@kotlinx.serialization.Serializable
+data class UserDetail(val uuid: String)
