32ea840838a667557d061abd7db89ba345049de9
gcaguilar <38892827+gcaguilar@users.noreply.github.com>
2025-04-27 09:20:11 +0200

    feat: update logic of delete user

--- database/src/main/java/com/gcaguilar/randomuser/database/dao/DeletedDao.kt
+++ database/src/main/java/com/gcaguilar/randomuser/database/dao/DeletedDao.kt
@@ -5,12 +5,11 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.gcaguilar.randomuser.database.entity.DeleteEntity
-import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface DeletedDao {
     @Query("SELECT * FROM deleted")
-    fun getAllDeletedUsers(): Flow<List<DeleteEntity>>
+    fun getAllDeletedUsers(): List<DeleteEntity>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(deleteEntity: DeleteEntity)
