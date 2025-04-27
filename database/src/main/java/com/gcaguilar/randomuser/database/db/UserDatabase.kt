fa6d66fd8e40d8b85e3d95b9fa86634186d61b2e
gcaguilar <38892827+gcaguilar@users.noreply.github.com>
2025-04-27 18:42:36 +0200

    style: spacing

--- database/src/main/java/com/gcaguilar/randomuser/database/db/UserDatabase.kt
+++ database/src/main/java/com/gcaguilar/randomuser/database/db/UserDatabase.kt
@@ -7,7 +7,7 @@ import com.gcaguilar.randomuser.database.dao.UserDao
 import com.gcaguilar.randomuser.database.entity.DeleteEntity
 import com.gcaguilar.randomuser.database.entity.UserEntity
 
-@Database(entities = [UserEntity::class, DeleteEntity::class],version = 1 , exportSchema = false)
+@Database(entities = [UserEntity::class, DeleteEntity::class], version = 1, exportSchema = false)
 abstract class UserDatabase : RoomDatabase() {
 
     abstract fun userDao(): UserDao
