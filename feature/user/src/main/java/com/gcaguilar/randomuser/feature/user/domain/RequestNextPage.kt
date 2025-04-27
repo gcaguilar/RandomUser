d8684c71a69ae001c1d81a86797104ac1f6f14f5
gcaguilar <38892827+gcaguilar@users.noreply.github.com>
2025-04-27 09:21:11 +0200

    feat: just insert users that already are not deleted

--- feature/user/src/main/java/com/gcaguilar/randomuser/feature/user/domain/RequestNextPage.kt
+++ feature/user/src/main/java/com/gcaguilar/randomuser/feature/user/domain/RequestNextPage.kt
@@ -1,20 +1,11 @@
 package com.gcaguilar.randomuser.feature.user.domain
 
-import android.util.Log
 import com.gcaguilar.randomuser.feature.user.data.repository.RandomUserRepository
-import kotlinx.io.IOException
 
 class RequestNextPage(
     private val randomUserRepository: RandomUserRepository
 ) {
-    suspend fun invoke(page: Int, seed: String) {
-        randomUserRepository.getPage(page, seed)
-            .recover {
-                Log.e(
-                    RequestNextPage::class.simpleName,
-                    it.message ?: "Failed to request next page"
-                )
-                IOException("Failed to request next page")
-            }
+    suspend operator fun invoke(page: Int, seed: String): Result<Unit> {
+        return randomUserRepository.getPage(page, seed)
     }
 }
